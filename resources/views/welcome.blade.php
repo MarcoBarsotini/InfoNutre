c7810c31a73669024f7b7de2c3394affd34f7f1f
Marco Barsotini <marcobarsotini@icloud.com>
2022-08-28 10:32:55 -0300

    Arrumando algumas coisas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+
+    <!-- Conexão dos arquivos CSS -->
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/general.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/homecss/home.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/homecss/navbar.css') }}">
+
+    <!-- Fim dos arquivos CSS -->
+
+
+    <title>InfoNutre - Home</title>
 </head>
 <body>
-    
-    <p> AAA </p>
+
+<!-- Começo do Body -->
+    <nav class="navbar">
+        <h1> aaaa</h1>
+    </nav>
+
+    <p> Olá </p>
+
+<!-- Fim do Body -->
+
+
+<!-- Começo do Rodapé --> 
+
+    <section>
+        <div class="rodape">
+            <p> AAA </p>
+        </div>
+    </section>
+
+<!-- Fim do Rodapé --> 
 
 </body>
 </html>
