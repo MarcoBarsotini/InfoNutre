aa92b4752b364eaac2f8ad0d4af1db4a848bb88f
MarcoBarsotini <marcobarsotini@icloud.com>
2022-08-30 12:08:14 -0300

    New Error screen

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>500 | Server Error</title>
+</head>
+<body>
+
+    <div class="box">
+        <a> ola </a>
+    </div>
+
+    <style>
+
+        html {
+            height: 100%;
+            padding: 0;
+            margin: 0;
+        }
+
+        .box {
+            display: flex;
+            align-content: flex-end;
+            
+            margin: 10px;
+            margin-top: 5%;
+            margin-left: 70%;
+            margin-right: 10%;
+
+        }
+
+        .titulos {
+            font-size: 15px;
+            
+        }
+
+        body {
+            background-color: white;
+            background-image: url("/images/svg/errors/500_server_error.svg");
+            background-repeat: no-repeat;
+            background-position: 5% 35%; 
+        }
+        
+
+    </style>
+    
+</body>
+</html>
