3ea8eacc49267fd5080bddbd2ed3a98ee00df427
MarcoBarsotini <marcobarsotini@icloud.com>
2022-09-12 12:10:52 -0300

    Card add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/general.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/homecss/home.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/homecss/navbar.css') }}">
-
+        <!-- Bootstrap Conection -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <!-- Fim dos arquivos CSS -->
@@ -41,16 +41,27 @@
 
     <br><br><br><br><br><br><br><br><br><br>
     <section id="cardapio"> 
-        <div class="container_cards">
-            <div class="card" style="width: 18rem;">
-                <img src="{{ asset('images/food/coxinha.png') }}" class="card-img-top" alt="Foto de uma Coxinha de Frango">
-            <div class="card-body">
-                <h5 class="card-title">Coxinha de Frango</h5>
-                <p class="card-text">Um delicioso salgado, recheado com Frango e Catupiry.</p>
-                <button type="button" class="btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#modalCoxinha"> Informações </button>
-                <button type="button" class="btn btn-primary btn-sm">Comprar</button>
+            <div class="container_cards">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/food/coxinha.png') }}" class="card-img-top" alt="Foto de uma Coxinha de Frango">
+                <div class="card-body">
+                    <h5 class="card-title"><b>Coxinha de Frango <br> R$5,00</h5></b>
+                    <p class="card-text">Um delicioso salgado, recheado com Frango e Catupiry.</p>
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#modalCoxinha"> Informações </button>
+                    <button type="button" class="btn btn-primary btn-sm">Comprar</button>
+                </div>
             </div>
-        </div>
+
+            <div class="container_cards">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/food/coca_2l.png') }}" class="card-img-top" alt="Foto de uma Coxinha de Frango">
+                <div class="card-body">
+                    <h5 class="card-title"><b>Coca Cola - 2L <br> R$10,00</h5></b>
+                    <p class="card-text">Garrafa de Coca Cola, 2 Litros não retornável.</p>
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#modalCoca"> Informações </button>
+                    <button type="button" class="btn btn-primary btn-sm">Comprar</button>
+                </div>
+            </div>
         </div>
     </section>
 <!-- Fim do Body -->
@@ -60,7 +71,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><b>Coxinha de Frango</b></h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><b>Coxinha de Frango - R$5,00</b></h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
             <div class="modal-body">
@@ -68,7 +79,31 @@
                 <b> Valores calóricos: </b> <br>
                 - 274 kcal; <br>
                 - 11,9 g de gordura; <br> <br>
-                <span> <b>Atenção! os valores podem variar, esta é apenas uma média.</b></span>
+                <span> <b>Atenção! os valores são apenas uma média.</b></span>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalCoca" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><b>Coca 2Litros - R$10,00</b></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            <div class="modal-body">
+                Está com sede, e vontade de beber algo para se refrescar? Sem problemas, uma coquinha gelada sempre resolve tudo. <br> <br>
+                <b> Valores calóricos: </b> <br>
+                - 60 kcal; <br>
+                - 15g Carboidratos; <br>
+                - 10mg Sódio; <br>
+                - 11,9 g de gordura; <br> <br>
+                <span> <b>Atenção! os valores são apenas uma média.</b></span>
 
             </div>
             <div class="modal-footer">
@@ -90,6 +125,7 @@
 <!-- Fim do Rodapé --> 
 
     <script type="text/javascript" src="{{ asset('js/homejs/home.js') }}"></script>
+    <!-- Bootstrap Conection -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
