d830eaf022cc265d0a095a66380eb6313e27901a
Marco Barsotini <marcobarsotini@icloud.com>
2022-09-16 11:38:41 -0300

    Some fixes and Content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,11 +71,21 @@
 
     <section class="container border border-opacity-50 rounded-3">
         <div class="item">
-            <img src="{{ asset('images/home/delivery.png') }}"/>
+            <img src="{{ asset('images/home/delivery2.png') }}"/>
         </div>
         <div class="item">
             <h1 class="fw-semibold fs-1"> Rápido e Eficiente </h1>
             <p> Cansado de pegar filas e ficar esperando sua vez chegar? <br> Com nosso sistema integrado, é possível comprar seus produtos com antecedência.</p>
+        </div>
+    </section>
+
+    <section class="container border border-opacity-50 rounded-3">
+        <div class="item">
+            <h1 class="fw-semibold fs-1"> Rápido e Eficiente </h1>
+            <p> Cansado de pegar filas e ficar esperando sua vez chegar? <br> Com nosso sistema integrado, é possível comprar seus produtos com antecedência.</p>
+        </div>
+        <div class="item">
+            <img src="{{ asset('images/home/delivery.png') }}"/>
         </div>
     </section>
     
