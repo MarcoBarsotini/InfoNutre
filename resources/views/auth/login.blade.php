8460f9d5a3c5b4c0a4aa59d8109e1491015392fa
MarcoBarsotini <marcobarsotini@icloud.com>
2022-09-28 08:09:57 -0300

    Tela de login em produção

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,38 @@
 @extends('layouts.auth-master')
 
 @section('content')
-    <form method="post" action="{{ route('login.perform') }}">
-        
-        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-        
-        <h1 class="h3 mb-3 fw-normal">Login</h1>
 
-        @include('layouts.partials.messages')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/logincss/main.css') }}">
 
-        <div class="form-group form-floating mb-3">
-            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
-            <label for="floatingName">Email ou Usuário </label>
-            @if ($errors->has('username'))
-                <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-            @endif
+    <div class="d-flex justify-content-center">
+        <div class="formulario">
+            <form method="post" action="{{ route('login.perform') }}">
+                
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                
+                <img src="{{ asset('images/logo_in.png') }}"/> 
+
+                @include('layouts.partials.messages')
+
+                <div class="form-group form-floating mb-3">
+                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+                    <label for="floatingName">Email ou Usuário </label>
+                    @if ($errors->has('username'))
+                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                    @endif
+                </div>
+                
+                <div class="form-group form-floating mb-3">
+                    <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+                    <label for="floatingPassword">Senha</label>
+                    @if ($errors->has('password'))
+                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+                
+            </form>
         </div>
-        
-        <div class="form-group form-floating mb-3">
-            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
-            <label for="floatingPassword">Senha</label>
-            @if ($errors->has('password'))
-                <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-            @endif
-        </div>
-
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-        
-    </form>
+    </div>
 @endsection
--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -10,25 +10,7 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
     
